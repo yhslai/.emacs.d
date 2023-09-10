3d0b90f37b4b8c3a6eefb9f6232cf89147832806
yhslai <raincolee@gmail.com>
2023-09-10 17:26:37 +0800

    Maybe it's a good idea to commit custom.el too

---
+++ custom.el
@@ -0,0 +1,15 @@
+
+;; Auto-generated. Don't touch these.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(corfu orderless vertico projectile yaml-mode tree-sitter lua-mode languagetool helpful undo-tree visual-fill-column org-ai evil-collection magit which-key evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
