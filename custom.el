0db4eb548ee5e211dada6fbfa2bceca7a16efec1
yhslai <raincolee@gmail.com>
2023-09-11 15:43:42 +0800

    Compact mode line

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line-compact 'long)
  '(package-selected-packages
    '(corfu orderless vertico projectile yaml-mode tree-sitter lua-mode languagetool helpful undo-tree visual-fill-column org-ai evil-collection magit which-key evil))
  '(send-mail-function 'mailclient-send-it))
